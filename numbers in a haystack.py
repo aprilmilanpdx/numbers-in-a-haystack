999dd75a5a6b07a9b2ac52a1ddf686181c422253
April Milan <108895508+aprilmilanpdx@users.noreply.github.com>
2023-01-09 17:44:35 -0800

    adding python and text files

---
+++ numbers in a haystack.py
@@ -0,0 +1,18 @@
+import re
+hand = open('C:\\Users\\april\\Desktop\\python\\actual_data.txt')
+numlist = []
+
+for line in hand:
+  line = line.rstrip()
+  stuff = re.findall('[0-9]+', line)
+  
+  if len(stuff) < 1:
+    continue
+  
+  for string in stuff:
+    numlist.append(int(string))
+print(sum(numlist))
+# sum = sum(stuff)
+# print(sum)
+
+# The basic outline of this problem is to read the file, look for integers using the re.findall(), looking for a regular expression of '[0-9]+' and then converting the extracted strings to integers and summing up the integers.
